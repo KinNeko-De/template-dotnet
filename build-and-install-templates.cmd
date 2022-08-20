1ea43d4175b65e031f4ed68ab88389e6f4bee1bc
Nils Diekmann <nils@diekmann-germany.de>
2022-08-20 14:49:31 +0200

    - updated readme - updated cmd for local developement - add support for version-suffix

--- build-and-install-templates.cmd
+++ build-and-install-templates.cmd
@@ -1,5 +1,5 @@
-del kinneko-de.template.dotnet.1.0.0.nupkg
+del kinneko-de.template.dotnet.0.1.0-local.nupkg
 dotnet new --uninstall kinneko-de.template.dotnet
-dotnet pack .\templatepack.csproj -o .
-dotnet new -i .\kinneko-de.template.dotnet.1.0.0.nupkg
+dotnet pack .\templatepack.csproj -o . --version-suffix local
+dotnet new -i .\kinneko-de.template.dotnet.0.1.0-local.nupkg
 pause
