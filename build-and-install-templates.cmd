2ba280924f58955974cf466f5a1f658084407cf7
Nils Diekmann <nils@diekmann-germany.de>
2022-11-20 17:16:22 +0100

    - upgrade package - upgrade to new dotnet new syntax

--- build-and-install-templates.cmd
+++ build-and-install-templates.cmd
@@ -1,5 +1,5 @@
 del kinneko-de.template.dotnet.0.1.0-local.nupkg
-dotnet new --uninstall kinneko-de.template.dotnet
+dotnet new uninstall kinneko-de.template.dotnet
 dotnet pack .\KinNeko-De.Template.Dotnet.csproj -o . --version-suffix local
-dotnet new -i .\kinneko-de.template.dotnet.0.1.0-local.nupkg
+dotnet new install .\kinneko-de.template.dotnet.0.1.0-local.nupkg
 pause
