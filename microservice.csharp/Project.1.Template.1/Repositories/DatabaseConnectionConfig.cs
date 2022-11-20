b745cadd1e543b23f372a53e2f0d00b575016260
Nils Diekmann <50811459+KinNeko-De@users.noreply.github.com>
2022-11-20 17:18:36 +0100

    Net 7 (#14)
    * upgrade to net 7
    * skip duplicate package in case of pipeline rerun
    * upgrade package
    * upgrade to new dotnet new syntax

--- microservice.csharp/Project.1.Template.1/Repositories/DatabaseConnectionConfig.cs
+++ microservice.csharp/Project.1.Template.1/Repositories/DatabaseConnectionConfig.cs
@@ -4,23 +4,23 @@ namespace Project._1.Template._1.Repositories;
 
 public class DatabaseConnectionConfig
 {
-    public string? Host { get; set; }
+    public required string? Host { get; set; }
 
-    public int Port { get; set; }
+    public required int Port { get; set; }
 
-    public string? Database { get; set; }
+    public required string? Database { get; set; }
 
-    public string? SearchPath { get; set; }
+    public required string? SearchPath { get; set; }
 
-    public string? User { get; set; }
+    public required string? User { get; set; }
 
-    public string? Password { get; set; }
+    public required string? Password { get; set; }
 
-    public SslMode SslMode { get; set; }
+    public required SslMode SslMode { get; set; }
 
-    public bool TrustServerCertificate { get; set; }
+    public required bool TrustServerCertificate { get; set; }
 
-    public int MaxPoolSize { get; set; }
+    public required int MaxPoolSize { get; set; }
 
-    public int Timeout { get; set; }
+    public required int Timeout { get; set; }
 }
